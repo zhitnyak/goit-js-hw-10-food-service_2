64fe2065e81a5a2558d071e5bb19c7169e16df29
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-09-15 23:47:01 +0300

    fix_bug

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import menu from './menu.json';
 import cardTpl from './templates/menu-card.hbs';
 
 const itemForCards = document.querySelector('.js-menu');
-console.log(cardsMarkup);
 const cardsMarkup = createCardsMarkup(menu);
 function createCardsMarkup(el) {
+  console.log(el);
   return el.map(cardTpl).join('');
 }
 itemForCards.insertAdjacentHTML('beforeend', cardsMarkup);
