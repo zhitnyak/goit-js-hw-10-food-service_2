6b24dde738130a9b4787ec752941896862715f95
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-09-19 15:01:03 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -8,50 +8,33 @@ const itemForCards = document.querySelector('.js-menu');
 
 const cardsMarkup = createCardsMarkup(menu);
 function createCardsMarkup(el) {
-  // console.log(el);
   return el.map(cardTpl).join('');
 }
 itemForCards.insertAdjacentHTML('beforeend', cardsMarkup);
+
+check.addEventListener('change', onChangeTheme);
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-
-check.addEventListener('change', onChangeTheme);
-
 const light = Theme.LIGHT;
 const dark = Theme.DARK;
-// localStorage.clear();
-console.dir(check);
+const saveLocalStorage = localStorage.getItem('Theme');
+
+if (saveLocalStorage === 'dark-theme') {
+  check.checked = true;
+  body.classList.add('dark-theme');
+}
 function onChangeTheme() {
-  if (localStorage.getItem('Theme') === light) {
-    // localStorage.setItem('Theme', light);
-
-    body.classList.add('light-theme');
-    body.classList.remove('dark-theme');
-  }
-  if (localStorage.getItem('Theme') === dark) {
-    // localStorage.setItem('Theme', dark);
-
-    body.classList.add('dark-theme');
-    body.classList.remove('light-theme');
-  }
   if (check.checked) {
     localStorage.setItem('Theme', dark);
     body.classList.add('dark-theme');
     body.classList.remove('light-theme');
-    console.log(localStorage.setItem('Theme', dark));
   }
   if (!check.checked) {
     localStorage.setItem('Theme', light);
     body.classList.add('light-theme');
     body.classList.remove('dark-theme');
-
-    console.log(localStorage.setItem('Theme', light));
   }
 }
-
-console.log(body);
-console.dir(check.value);
-console.log(localStorage.getItem('Theme'));
